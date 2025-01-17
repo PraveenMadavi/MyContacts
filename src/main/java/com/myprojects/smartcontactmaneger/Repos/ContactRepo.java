0c90f6ef4b6f3df8e84bef5e2ff6ed5939e8de4a
PraveenMadavi <madavione@gmail.com>
2025-01-17 23:00:38 +0530

    Two Resources added.

--- src/main/java/com/myprojects/smartcontactmaneger/Repos/ContactRepo.java
+++ src/main/java/com/myprojects/smartcontactmaneger/Repos/ContactRepo.java
@@ -1,9 +1,16 @@
 package com.myprojects.smartcontactmaneger.Repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.myprojects.smartcontactmaneger.entities.Contact;
 
-public interface ContactRepo  extends JpaRepository <Contact , Integer>{
-    
+public interface ContactRepo extends JpaRepository<Contact, Integer> {
+
+    @Query("SELECT c FROM Contact c WHERE c.user.id = :userId")
+    List<Contact> getContacts(@Param("userId") int userId);
+
 }
--- src/main/java/com/myprojects/smartcontactmaneger/controllers/UserController.java
+++ src/main/java/com/myprojects/smartcontactmaneger/controllers/UserController.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.myprojects.smartcontactmaneger.Repos.ContactRepo;
 import com.myprojects.smartcontactmaneger.Repos.UserRepo;
 import com.myprojects.smartcontactmaneger.entities.Contact;
+import com.myprojects.smartcontactmaneger.entities.Message;
 import com.myprojects.smartcontactmaneger.entities.User;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/user")
@@ -31,43 +31,68 @@ public class UserController {
     private User user;
 
     @ModelAttribute
-    public void Commonhandler( Model model,Principal principal){
+    public void Commonhandler(Model model, Principal principal) {
         String userName = principal.getName();
         String userdetails = principal.toString();
         model.addAttribute("username", userName);
-        model.addAttribute("userdetails",userdetails);
+        model.addAttribute("userdetails", userdetails);
 
         user = userRepo.getUserByEmail(userName);
-         model.addAttribute("user", user);
+        model.addAttribute("user", user);
     }
 
     @RequestMapping("/index")
-    public String dashbourd() {
-        
+    public String dashbourd(Model model) {
+        model.addAttribute("title", "User Dashboard");
         // System.out.println(userdetails);
-    
+
         return "normal/dashbourd";
     }
 
     @RequestMapping("/addContact")
-    public String addContact(Model model) {  
-        model.addAttribute("contact", new Contact());  
+    public String addContact(Model model) {
+        model.addAttribute("contact", new Contact());
+        model.addAttribute("title", "Add Contact");
+
         System.out.println("APPLICATION : Add contact form triggered.");
         return "normal/addcontactform";
     }
 
-
-
-    @PostMapping("/addNewContact") // Process method for adding new contact
-    public String addNewContact(@ModelAttribute ("contact") Contact contact, BindingResult result, Model  model) { 
+    @PostMapping("/addNewContact") // Processing method for adding new contact
+    public String addNewContact(@ModelAttribute("contact") Contact contact, BindingResult result, Model model) {
 
         System.out.println("APPLICATION : New Contact");
         contact.setUser(user);
         System.out.println(contact.toString());
         contactRepo.save(contact);
+        model.addAttribute("message", new Message("Contact added successfully.", "alert-primary"));
+        model.addAttribute("title", "UserDashboard");
 
         return "normal/addcontactform";
     }
-    
-    
+
+    @GetMapping("/mycontacts") // Processing method for getting my contacts
+    public String myContacts(Model model) {
+        List<Contact> contacts = contactRepo.getContacts(user.getId());
+
+        model.addAttribute("contacts", contacts);
+
+        if (!contacts.isEmpty()) {
+            System.out.println("APPLICATION : My Contacts ~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            for (Contact contact : contacts) {
+                System.out.println(contact);
+                System.out.println("----------------------------------------");
+            }
+        }
+
+        if (contacts.isEmpty()) {
+            model.addAttribute("message", "No contacts found");
+        }
+
+        model.addAttribute("title", "My Contacts");
+
+        return "normal/mycontacts";
+
+    }
+
 }
