1a02690913d4f59d53ae1633417456e1b941daba
PraveenMadavi <madavione@gmail.com>
2025-01-17 23:20:17 +0530

    Logger added

--- src/main/java/com/myprojects/smartcontactmaneger/SmartcontactmanegerApplication.java
+++ src/main/java/com/myprojects/smartcontactmaneger/SmartcontactmanegerApplication.java
@@ -1,16 +1,18 @@
 package com.myprojects.smartcontactmaneger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartcontactmanegerApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SmartcontactmanegerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartcontactmanegerApplication.class, args);
 
-		System.out.println(
-				"Smartcontactmaneger application started ===================================================================>");
+		logger.info("Smartcontactmaneger application started...");
 	}
 
 }
